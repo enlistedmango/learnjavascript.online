c41d58898548fb7c9185706b02035ca7e335a536
Mango <82413197+enlistedmango@users.noreply.github.com>
2022-10-30 21:21:57 +0000

    Finished Numbers Section

---
+++ 03. Numbers/get_division_remainder_by_2.js
@@ -0,0 +1,14 @@
+/*
+Complete the function getDivisionRemainderBy2 such 
+that it returns the division remainder of the number 
+it receives by 2. This means that it should return the division 
+remainder by 2.
+*/
+
+
+/**
+ * @param {string} number
+ */
+ export function getDivisionRemainderBy2(number) {
+    return number % 2;
+}
